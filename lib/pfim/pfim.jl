dabdf3cca1a59c757401cca68d97c27e9f4bfe42
Tanya Strydom <tanya.strydom@icloud.com>
2024-06-25 12:37:07 +0100

    🧼 formatting

--- lib/pfim/pfim.jl
+++ lib/pfim/pfim.jl
@@ -13,9 +13,9 @@ end
 
 # create community with appropraite types
 function _pfim_community(data::DataFrame)
-    
 
-    PFIMcommunity = Array{PFIMspecies, 1}(undef, nrow(data))
+
+    PFIMcommunity = Array{PFIMspecies,1}(undef, nrow(data))
 
     for i in eachindex(PFIMcommunity)
         _PFIMsp = PFIMspecies(
@@ -23,8 +23,8 @@ function _pfim_community(data::DataFrame)
             getfield(Main, Symbol(data.feeding[i]))(),
             getfield(Main, Symbol(data.size[i]))(),
             getfield(Main, Symbol(data.motility[i]))(),
-            getfield(Main, Symbol(data.tiering[i]))()
-            )
+            getfield(Main, Symbol(data.tiering[i]))(),
+        )
         PFIMcommunity[i] = _PFIMsp
     end
     return PFIMcommunity
@@ -33,8 +33,9 @@ end
 # determine link feasibility
 function _pfim_link(consumer::PFIMspecies, resource::PFIMspecies)
 
-    trait_sum = feeding_rules(consumer.feeding_trait, resource.feeding_trait) + 
-        motility_rules(consumer.motility_trait, resource.motility_trait) + 
+    trait_sum =
+        feeding_rules(consumer.feeding_trait, resource.feeding_trait) +
+        motility_rules(consumer.motility_trait, resource.motility_trait) +
         tiering_rules(consumer.tiering_trait, resource.tiering_trait) +
         size_rules(consumer.size_trait, resource.size_trait)
 
@@ -54,10 +55,10 @@ function _PFIM_network(PFIMcommunity::Vector{PFIMspecies})
 
     for i in eachindex(PFIMcommunity)
         for j in eachindex(PFIMcommunity)
-            int_matrix[i,j] = _pfim_link(PFIMcommunity[i], PFIMcommunity[j])
-        end  
+            int_matrix[i, j] = _pfim_link(PFIMcommunity[i], PFIMcommunity[j])
+        end
     end
-    
+
     nodes = Unipartite(getproperty.(PFIMcommunity, :species))
     edges = Binary(int_matrix)
     network = SpeciesInteractionNetwork(nodes, edges)
@@ -66,10 +67,7 @@ end
 
 # downsample
 
-function PFIM(
-    data::DataFrame;
-    y::Float64 = 2.5
-    )
+function PFIM(data::DataFrame; y::Float64 = 2.5)
 
     S = nrow(data)
     PFIMcommunity = _pfim_community(data)
@@ -80,8 +78,8 @@ function PFIM(
     for i in eachindex(data.species)
         sp = data.species[i]
         r = generality(network, Symbol(sp))
-        E = exp(log(S)*(y-1)/y)
-        link_dist[i] = exp(r/E)
+        E = exp(log(S) * (y - 1) / y)
+        link_dist[i] = exp(r / E)
     end
 
     # create probabilistic int matrix
@@ -89,13 +87,13 @@ function PFIM(
     for i in axes(matrix, 1)
         for j in axes(matrix, 2)
             if matrix[i, j] == true
-            prob_matrix[i, j] = link_dist[i]
+                prob_matrix[i, j] = link_dist[i]
+            end
         end
     end
-end
 
     # make probabanilistic
-    prob_matrix = prob_matrix./maximum(prob_matrix)
+    prob_matrix = prob_matrix ./ maximum(prob_matrix)
 
     edges = Probabilistic(prob_matrix)
     nodes = Unipartite(Symbol.(data.species))
--- lib/pfim/rules.jl
+++ lib/pfim/rules.jl
@@ -3,7 +3,7 @@ include(joinpath("types.jl"))
 # Define rules for each trait type
 
 # feeding rules
-feeding_rules(consumer::T1, resource::T2) where {T1 <: feeding, T2 <: feeding} = 0
+feeding_rules(consumer::T1, resource::T2) where {T1<:feeding,T2<:feeding} = 0
 feeding_rules(consumer::predator, resource::suspension) = 1
 feeding_rules(consumer::predator, resource::surficial) = 1
 feeding_rules(consumer::predator, resource::predator) = 1
@@ -11,14 +11,14 @@ feeding_rules(consumer::suspension, resource::producer) = 1
 feeding_rules(consumer::surficial, resource::producer) = 1
 
 # motility rules
-motility_rules(consumer::T1, resource::T2) where {T1 <: motility, T2 <: motility} = 0
-motility_rules(consumer::motile_fast, resource::T) where {T <: motility} = 1
+motility_rules(consumer::T1, resource::T2) where {T1<:motility,T2<:motility} = 0
+motility_rules(consumer::motile_fast, resource::T) where {T<:motility} = 1
 motility_rules(consumer::motile_fast, resource::motile_fast) = 1
-motility_rules(consumer::motile_slow, resource::T) where {T <: motility} = 1
+motility_rules(consumer::motile_slow, resource::T) where {T<:motility} = 1
 motility_rules(consumer::motile_slow, resource::motile_fast) = 0
-motility_rules(consumer::motile_slow, resource::T) where {T <: motility} = 1
+motility_rules(consumer::motile_slow, resource::T) where {T<:motility} = 1
 motility_rules(consumer::facultative_unattached, resource::motile_fast) = 0
-motility_rules(consumer::facultative_unattached, resource::T) where {T <: motility} = 1
+motility_rules(consumer::facultative_unattached, resource::T) where {T<:motility} = 1
 motility_rules(consumer::facultative_attached, resource::facultative_unattached) = 1
 motility_rules(consumer::facultative_attached, resource::motile_slow) = 1
 motility_rules(consumer::facultative_attached, resource::nonmotile_unattached) = 1
@@ -26,24 +26,24 @@ motility_rules(consumer::nonmotile_unattached, resource::nonmotile_unattached) =
 motility_rules(consumer::nonmotile_attached, resource::nonmotile_unattached) = 1
 
 # tiering rules
-tiering_rules(consumer::T1, resource::T2) where {T1 <: tier, T2 <: tier} = 0
-tiering_rules(consumer::semi_aquatic, resource::T) where {T <: tier} = 1
+tiering_rules(consumer::T1, resource::T2) where {T1<:tier,T2<:tier} = 0
+tiering_rules(consumer::semi_aquatic, resource::T) where {T<:tier} = 1
 tiering_rules(consumer::semi_aquatic, resource::semi_infaunal) = 0
 tiering_rules(consumer::semi_aquatic, resource::infaunal) = 0
-tiering_rules(consumer::pelagic, resource::T) where {T <: tier} = 1
+tiering_rules(consumer::pelagic, resource::T) where {T<:tier} = 1
 tiering_rules(consumer::pelagic, resource::infaunal) = 0
-tiering_rules(consumer::erect, resource::T) where {T <: tier} = 1
+tiering_rules(consumer::erect, resource::T) where {T<:tier} = 1
 tiering_rules(consumer::erect, resource::semi_aquatic) = 0
 tiering_rules(consumer::erect, resource::infaunal) = 0
-tiering_rules(consumer::semi_infaunal, resource::T) where {T <: tier} = 1
+tiering_rules(consumer::semi_infaunal, resource::T) where {T<:tier} = 1
 tiering_rules(consumer::semi_infaunal, resource::semi_aquatic) = 0
 tiering_rules(consumer::semi_infaunal, resource::pelagic) = 0
 tiering_rules(consumer::infaunal, resource::infaunal) = 1
 tiering_rules(consumer::infaunal, resource::semi_infaunal) = 1
 
 # size rules
-size_rules(consumer::T1, resource::T2) where {T1 <: sizes, T2 <: sizes} = 0
-size_rules(consumer::very_large, resource::T) where {T <: sizes} = 1
+size_rules(consumer::T1, resource::T2) where {T1<:sizes,T2<:sizes} = 0
+size_rules(consumer::very_large, resource::T) where {T<:sizes} = 1
 size_rules(consumer::large, resource::medium) = 1
 size_rules(consumer::large, resource::small) = 1
 size_rules(consumer::medium, resource::small) = 1
