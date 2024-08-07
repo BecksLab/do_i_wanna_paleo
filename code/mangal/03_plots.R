03d21496c90b6c4edcca2b76800332d685e4c746
Tanya Strydom <tanya.strydom@icloud.com>
2024-08-07 13:24:40 +0100

    :sparkles: distributions

--- code/mangal/03_plots.R
+++ code/mangal/03_plots.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
 df <- read.csv("data/processed/mangal_summary.csv") %>% 
+    mutate(intermediate = (richness - top - basal)/richness,
+            basal = basal/richness,
+            top = top/richness) %>% 
     pivot_longer(!id,
                 names_to = "statistic",
                 values_to = "val")
@@ -10,3 +13,5 @@ ggplot(df, aes(val)) +
     facet_wrap(vars(statistic),
                 scales = "free") +
     theme_classic()
+
+ggsave("figures/distributions.png")
